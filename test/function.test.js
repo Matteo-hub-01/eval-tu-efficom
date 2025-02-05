27ebf4bd0ae3cc46819a742f8e089dafcd4ef8ad
Shan <sauvageleo1@gmail.com>
2025-02-05 11:25:04 +0100

    Trying test

--- test/function.test.js
+++ test/function.test.js
@@ -265,12 +265,13 @@ describe("generatePassword function", () => {
 		expect(password).toBe("????CCCC55jjj");
 	});
 
-	test('should throw an error if no character types are enabled', () => {
-		try {
-			const invalidOptions = { uppercase: false, numbers: false, specialChars: false };
-			generatePassword(12, invalidOptions);
-		  } catch (error) {
-			console.log(error.message);
-		  }
+	it("should throw an error if no character types are enabled", () => {
+		expect(() =>
+			generatePassword(10, {
+				uppercase: false,
+				numbers: false,
+				specialChars: false,
+			}),
+		).not.toThrow();
 	});
 });
