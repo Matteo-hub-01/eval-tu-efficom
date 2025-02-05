992dd05530a30de2eccad76efeedfa0a49f5f38f
unknown <mcours@LAPFRFL021.Proplast.fr>
2025-02-05 08:33:28 +0100

    premier exo

--- test/function.test.js
+++ test/function.test.js
@@ -1,5 +1,45 @@
 const { isEven, calculateTotalPrice, processPurchase, sendNotification, generatePassword } = require("./../src/function.js");
 
-test("",() =>{
+describe ('',()=>{
 
-});
+    test("",() =>{
+        try{
+            let result = isEven(4);
+        }
+        catch(e){
+            expect(e).not.toBeNull();
+            expect(e.message).toBe("Input must be a number");
+        } 
+    });
+
+
+    test("",() =>{
+        expect(isEven(4)).toBe(true);
+    });
+})
+
+describe ('',()=>{
+
+    test("",() =>{
+        try{
+            let result = calculateTotalPrice(4);
+        }
+        catch(e){
+            expect(e).not.toBeNull();
+            expect(e.message).toBe("Prices must be an array");
+        } 
+    });
+
+    test("",() =>{
+        try{
+            let result = calculateTotalPrice(4);
+        }
+        catch(e){
+            expect(e).not.toBeNull();
+            expect(e.message).toBe("Prices must be an array");
+        } 
+    });
+
+
+})
+
